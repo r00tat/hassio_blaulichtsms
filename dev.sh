c9d2af1c544d6c26a0b64e1e070f9c0136c7055c
Paul Woelfel <paul@woelfel.at>
2023-12-10 23:15:52 +0100

    Update dev script

--- dev.sh
+++ dev.sh
@@ -14,5 +14,18 @@ fi
 
 rsync -avz custom_components/blaulichtsms/ config/custom_components/blaulichtsms/
 
-docker restart homeassistant
+if [ ! "$(docker ps -a -q -f name=homeassistant)" ]; then
+  docker run -d \
+    --name homeassistant \
+    --privileged \
+    --restart=unless-stopped \
+    -e TZ=Europe/Vienna \
+    -v $PWD/config:/config \
+    -v /run/dbus:/run/dbus:ro \
+    -p 8123:8123 \
+    ghcr.io/home-assistant/home-assistant:stable
+
+else
+  docker restart homeassistant
+fi
 docker logs -n 10 -f homeassistant

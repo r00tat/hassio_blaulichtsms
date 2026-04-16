adc289469e79206dfb5ccf9d919392898a248cea
Paul Woelfel <paul@woelfel.at>
2026-04-16 20:46:59 +0200

    feat(blaulichtsms): add new_alarm_duration config constants

--- custom_components/blaulichtsms/constants.py
+++ custom_components/blaulichtsms/constants.py
@@ -8,9 +8,11 @@ CONF_PASSWORD = "password"
 CONF_ALARM_DURATION = "alarm_duration"
 CONF_SHOW_INFOS = "show_infos"
 CONF_TRACK_RECIPIENT = "track_recipient"
+CONF_NEW_ALARM_DURATION = "new_alarm_duration"
 
 
 DEFAULT_ALARM_DURATION = 3600
 DEFAULT_SHOW_INFOS = False
+DEFAULT_NEW_ALARM_DURATION = 300
 
 VERSION = "0.3.0"
